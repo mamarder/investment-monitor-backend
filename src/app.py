17ec85393fa86f57672778ff4d708ead521af78b
Marco Marder <marco.marder@gmx.de>
2025-02-23 14:18:49 +0100

    feat: renamed broker

--- src/app.py
+++ src/app.py
@@ -4,6 +4,7 @@ from Deposits import Deposits
 from Statements import Statements
 from flask_cors import CORS
 
+import json
 import logging
 import os
 
@@ -38,23 +39,42 @@ class Welcome(Resource):
 class Deposits(Resource): 
 
 	def get(self, broker : str): 
-		d = deposits.all_for(broker)
-		result = d.to_json(orient='records')
-		log.info(f'Found {len(d)} deposits for broker {broker}')
+		log.info(f"Deposits for {broker}")
+		s = deposits.all_for(broker)
+		result = s.to_json(orient='records')
+		log.info(f'Found {len(s)} deposits for broker {broker}')
 		return result
-
 
 class Statements(Resource): 
 
 	def get(self, broker : str): 
+		log.info(f"Statements for {broker}")
 		s = statements.all_for(broker)
 		result = s.to_json(orient='records')
 		log.info(f'Found {len(s)} statements for broker {broker}')
 		return result
 
+class Overview(Resource): 
 
-api.add_resource(Deposits, '/deposits/<string:broker>') 
-api.add_resource(Statements, '/statements/<string:broker>') 
+	def get(self, broker : str): 
+		log.info(f"Overview for {broker}")
+
+		(depos_src_value, depos_src_currency, sum_deposits, currency_deposits) = deposits.sum_for(broker)
+		latest_statement = statements.latest_for(broker)
+		latest_value = latest_statement["value"]
+		profit = (latest_value - sum_deposits)/sum_deposits
+
+		result = []
+		
+		result.append({"currency": currency_deposits, "value_deposits": sum_deposits, "value_latest": latest_value, "profit": profit})
+
+		return json.dumps(result)
+
+
+api.add_resource(Deposits, '/<string:broker>/deposits') 
+api.add_resource(Statements, '/<string:broker>/statements') 
+api.add_resource(Overview, '/<string:broker>/overview') 
+
 api.add_resource(Welcome, '/') 
 
 
